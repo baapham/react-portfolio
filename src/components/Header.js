52332cc7407de091c1b867545e34a35dd9593278
Alex Pham <z5209652@ad.unsw.edu.au>
2019-12-21 19:09:12 +1100

    styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 
 const Header = () => (
-    <div>
-        <h1>Alex Pham</h1>
-        <a href="#about">About</a>
-        <a href="#projects">Projects</a>
-    </div>
+    <header className="header">
+        <div className="content-container">
+            <div className = "header-container">
+                <div className="header__title">
+                    <h1>Alex Pham</h1>
+                </div>
+                <div className="header__links">
+                    <a href="#about" className="header__subtitle">About</a>
+                    <a href="#projects" className="header__subtitle">Projects</a>
+                </div>
+            </div>
+        </div>
+    </header>
 );
 
 export default Header;
--- src/components/PortfolioApp.js
+++ src/components/PortfolioApp.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Header from './Header';
 import About from './About';
 import Projects from './Projects';
-import Animation from './Animation';
 
 function App() {
   return (
     <div>
-        <Animation />
         <Header />
         <About id="about" />
         <Projects id="projects" />
