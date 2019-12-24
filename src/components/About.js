930ab845bc4c51acb27300b171572de6bdf5766c
Alex Pham <z5209652@ad.unsw.edu.au>
2019-12-24 16:56:12 +1100

    Added some about description

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,21 @@ const About = (props) => (
         <div className="about-main">
             <div className="content-container">
             <h2>About Me</h2>
-            <h3>What is Lorem Ipsum?</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <h3>Why do we use it?</h3>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <h3><i>Hey, I'm Alex</i></h3>
+            <p>I am currently a 3rd year UNSW student studying a double degree of Computer Science and Commerce (Finance).
+                In real life, I like to fiddle a lot, from shaking my leg to tapping my fingers - you name it, I do it. Does that make me annoying?
+                <b>Maybe.</b> I tried to make the most out of this habit by crafting an interactive portfolio website that you can fiddle with - play with the background, swing your mouse around,
+                do whatever your heart desires!
+            </p>
+            <h3><i>My Origin Story</i></h3>
+            <p>I was born in <strike>Gotham City</strike> <b>Sydney</b> and was first exposed to computers at an early age. My love of programming only really started
+            at the age 12 when <b>Minecraft</b> was released. From here it only grew stronger when I went on to play <b>Call of Duty: Modern Warfare</b> - I was able to 
+            help host one of the biggest Australian servers at that time (to be fair there was only about 5 Aussie servers) and form my own gaming clans with their own websites.
+            </p>
+
+            <p>After this, in Year 10 I competed in a <b>Python Coding Competition</b> and enjoyed it so much I decided to put my self a few thousand in debt
+            and get a Computer Science degree.</p>
+
         </div>
         </div>
     </div>
