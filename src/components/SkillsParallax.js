ec6ce2a41380d1c75e3bb2bcd258a8b38bf02e84
Alex Pham <z5209652@ad.unsw.edu.au>
2019-12-29 17:38:06 +1100

    i finished cloud styling finally (i think)

--- src/components/SkillsParallax.js
+++ src/components/SkillsParallax.js
@@ -3,36 +3,33 @@ import Parallax from 'parallax-js';
 class SkillsParallax extends React.Component {
     componentDidMount() {
         const scene = document.getElementById('scene');
-        const parallaxInstance = new Parallax(scene);
-        // const scene2 = document.getElementById('scene2');
-        // const parallaxInstance2 = new Parallax(scene2);
+        new Parallax(scene);
     }
     render() {
         return(
-            <ul id="scene" className="parallax-scene">
-                <li className="layer" data-depth="1">
-                    <ul className="cloud-position-1">
-                        <img src={require('../images/cloud-frontend.svg')} className="cloud"></img>
-                    </ul>
+            <div id="scene" className="parallax-scene" data-invert-y="false" data-invert-x="false" data-relative-input="true">
+                <div className="layer" data-depth="0.8">
+                    <div className="cloud-position-1">
+                        <img src={require('../images/cloud-frontend.svg')} className="cloud x1 cloud-shadow" alt="frontend"></img>
+                    </div>
+                </div>
+                <div className="layer" data-depth="0.4">
+                    <div className="cloud-position-3">
+                        <img src={require('../images/cloud-databases.svg')} className="cloud x3 cloud-shadow" alt="databases"></img>
+                    </div>
                     
-                </li>
-                <li className="layer" data-depth="0.4">
-                    <ul className="cloud-position-2">
-                        <img src={require('../images/cloud-services.svg')} className="cloud x2"></img>
-                    </ul>
-                </li>
-                <li className="layer" data-depth="0.4">
-                    <ul className="cloud-position-3">
-                        <img src={require('../images/cloud-databases.svg')} className="cloud x3"></img>
-                    </ul>
-                    
-                </li>
-                <li className="layer" data-depth="0.8">
-                    <ul className="cloud-position-4">
-                        <img src={require('../images/cloud-backend.svg')} className="cloud x2"></img>
-                    </ul>
-                </li>
-            </ul>
+                </div>
+                <div className="layer" data-depth="0.6">
+                    <div className="cloud-position-4">
+                        <img src={require('../images/cloud-backend.svg')} className="cloud x2 cloud-shadow" alt="backend"></img>
+                    </div>
+                </div>
+                <div className="layer" data-depth="1">
+                    <div className="cloud-position-2">
+                        <img src={require('../images/cloud-services.svg')} className="cloud cloud-shadow" alt="services"></img>
+                    </div>
+                </div>
+            </div>
             
         )
     }
