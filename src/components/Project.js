db45b1b9af11a350ec5fb45cd99423e205785afb
Alex Pham <z5209652@ad.unsw.edu.au>
2020-01-01 19:13:36 +1100

    project section added without modals

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const Project = () => (
-    <div>
-        <h3>This is a single project</h3>
+const Project = (props) => (
+    <div className="project">
+        <div className="project-image">
+            <img src={require(`../images/${props.imgSrc}`)} alt={props.imgSrc}></img>
+        </div>
+        <div className="project-content">
+            <div className="project-title">
+                {props.title}
+            </div>
+            <div className="project-tech">
+                {props.tech}
+            </div>
+            <button className="project-more">
+                <i className="arrow"></i> Find out more
+            </button>
+            
+        </div>
     </div>
 );
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,28 @@ const Projects = (props) => (
                 <div  id={props.id} className="section-heading">
                     <h1>Projects</h1>
                 </div>
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-                <Project />
+                <div className="projects-content">
+                    <Project 
+                        imgSrc="IMnotDb.png" 
+                        title="IMnotDb"
+                        tech="Bootstrap, CSS, HTML, Javascript"
+                    />
+                    <Project 
+                        imgSrc="AccessGESSS.png" 
+                        title="Access GESSS"
+                        tech="DigitalOcean, Google Maps API, MySQL, Wordpress"
+                    />
+                    <Project 
+                        imgSrc="ReactPortfolio.png" 
+                        title="React Portfolio"
+                        tech="CSS, Heroku, HTML, Javascript (ReactJS)"
+                    />
+                    <Project 
+                        imgSrc="Countdown.png" 
+                        title="Countdown"
+                        tech="Bootstrap, CSS, HTML, PHP"
+                    />
+                </div>
             </div>
         </div>
     </div>
