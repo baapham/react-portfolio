7264f86dc494a8eecbcf87acd10e468247a89ddc
Alex Pham <z5209652@ad.unsw.edu.au>
2020-01-01 19:48:35 +1100

    pls work

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import Project from './Project';
 import IMnotDB from '../images/IMnotDb.png';
 import AccessGESSS from '../images/AccessGESSS.png';
 import ReactPortfolio from '../images/ReactPortfolio.png';
-// import Countdown from '../images/Countdown.png';
+import Countdown from '../images/countdown.png';
 
 // represents the whole entire projects section
 const Projects = (props) => (
@@ -15,30 +15,25 @@ const Projects = (props) => (
                 </div>
                 <div className="projects-content">
                     <Project 
-                    // Had to pass it in like this because I was running into errors
-                        // imgSrc={require('../images/IMnotDb.png')}
                         imgSrc={IMnotDB}
                         title="IMnotDb"
                         tech="Bootstrap, CSS, HTML, Javascript"
                     />
                     <Project 
-                        // imgSrc={require('../images/AccessGESSS.png')}
                         imgSrc={AccessGESSS}
                         title="Access GESSS"
                         tech="DigitalOcean, Google Maps API, MySQL, Wordpress"
                     />
                     <Project 
-                        // imgSrc={require('../images/ReactPortfolio.png')}
                         imgSrc={ReactPortfolio}
                         title="React Portfolio"
                         tech="CSS, Heroku, HTML, Javascript (ReactJS)"
                     />
-                    {/* <Project 
-                        // imgSrc={require('../images/Countdown.png')}
+                    <Project 
                         imgSrc={Countdown}
                         title="Countdown"
                         tech="Bootstrap, CSS, HTML, PHP"
-                    /> */}
+                    />
                 </div>
             </div>
         </div>
