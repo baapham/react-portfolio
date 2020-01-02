52e90771a323d62d55d7dc1a9a4abe7d98e9a799
Alex Pham <z5209652@ad.unsw.edu.au>
2020-01-02 21:41:42 +1100

    testing background change

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Particles from 'react-particles-js';
-
+import { TriangleBG } from './Triangles.js';
 const particleParam = {
   particles: {
     number: {
@@ -42,16 +42,51 @@ class Background extends React.Component {
         window.scrollTo(0, aboutHeight - headerHeight);
     };
 
+    componentDidMount = () => {
+      const canvas = document.getElementById("triangle-canvas");
+        const altBG = document.getElementById("altBG");
+        new TriangleBG({
+            canvas : canvas,
+            alternateElem : altBG,
+            cellHeight : 120,
+            cellWidth : 100,
+            mouseLight : true,
+            mouseLightRadius : 200,
+            mouseLightIncrement : 40,
+            resizeAdjustment : true,
+            variance : 0.9,
+            pattern : "y",
+            baseColor1 : {
+                baseHue : 210,
+                baseSaturation : 74.8,
+                baseLightness : 58
+            },
+            baseColor2 : {
+                baseHue : 210,
+                baseSaturation : 74.8,
+                baseLightness : 56
+            },
+            colorDelta : {
+                hue : 2,
+                lightness : 0,
+                saturation : 0
+            }
+       });
+    }
     render() {
         return (
             <div className="particle-container">
-               <h1 className="particle-title">Alex Pham</h1>
-               <button className="particle-button" onClick={this.workClick}>View my work</button>
-            <Particles
+               
+            {/* <Particles
                 className="particle-background"
                 params={particleParam}
-            />  
-          </div>
+            />   */}
+                <canvas id="triangle-canvas">
+                </canvas>
+                <div id="altBG"></div>
+                <h1 className="particle-title">Alex Pham</h1>
+                <button className="particle-button" onClick={this.workClick}>View my work</button>
+            </div>
         );
     } 
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,35 +10,35 @@ export default class MyForm extends React.Component {
     }
 
     componentDidMount() {
-        const canvas = document.getElementById("triangle-canvas");
-        const altBG = document.getElementById("altBG");
-        new TriangleBG({
-            canvas : canvas,
-            alternateElem : altBG,
-            cellHeight : 120,
-            cellWidth : 100,
-            mouseLight : true,
-            mouseLightRadius : 200,
-            mouseLightIncrement : 40,
-            resizeAdjustment : true,
-            variance : 0.9,
-            pattern : "y",
-            baseColor1 : {
-                baseHue : 210,
-                baseSaturation : 74.8,
-                baseLightness : 58
-            },
-            baseColor2 : {
-                baseHue : 210,
-                baseSaturation : 74.8,
-                baseLightness : 56
-            },
-            colorDelta : {
-                hue : 2,
-                lightness : 0,
-                saturation : 0
-            }
-       });
+    //     const canvas = document.getElementById("triangle-canvas");
+    //     const altBG = document.getElementById("altBG");
+    //     new TriangleBG({
+    //         canvas : canvas,
+    //         alternateElem : altBG,
+    //         cellHeight : 120,
+    //         cellWidth : 100,
+    //         mouseLight : true,
+    //         mouseLightRadius : 200,
+    //         mouseLightIncrement : 40,
+    //         resizeAdjustment : true,
+    //         variance : 0.9,
+    //         pattern : "y",
+    //         baseColor1 : {
+    //             baseHue : 210,
+    //             baseSaturation : 74.8,
+    //             baseLightness : 58
+    //         },
+    //         baseColor2 : {
+    //             baseHue : 210,
+    //             baseSaturation : 74.8,
+    //             baseLightness : 56
+    //         },
+    //         colorDelta : {
+    //             hue : 2,
+    //             lightness : 0,
+    //             saturation : 0
+    //         }
+    //    });
     }
   
     submitForm(ev) {
@@ -65,9 +65,9 @@ export default class MyForm extends React.Component {
         const { status } = this.state;
         return (
             <div className="contact">
-                    <canvas id="triangle-canvas">
+                    {/* <canvas id="triangle-canvas">
                     </canvas>
-                    <div id="altBG"></div>
+                    <div id="altBG"></div> */}
                     <div className="triangle-container">
                             <form
                             onSubmit={this.submitForm}
