07e3e9b9be4180fad001ba701bf4dfda5e0b7bd2
Alex Pham <z5209652@ad.unsw.edu.au>
2020-02-25 21:00:25 +1100

    changed

--- src/components/PortfolioApp.js
+++ src/components/PortfolioApp.js
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactNotifications from "react-notifications-component";
-import ReactGa from "react-ga";
+import React from "react";
 import Header from "./Header";
 import About from "./About";
 import Projects from "./Projects";
@@ -10,10 +9,6 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 
 class App extends React.Component {
-  componentDidMount() {
-    ReactGa.initialize("UA-159039430-1");
-    ReactGa.pageview("/");
-  }
   render() {
     return (
       <div>
