667533ba1d82a248c939191c5762c64f0a5dc321
Alex Pham <z5209652@ad.unsw.edu.au>
2020-02-25 20:46:47 +1100

    chagned analytics

--- src/components/PortfolioApp.js
+++ src/components/PortfolioApp.js
@@ -1,28 +1,33 @@
-import ReactNotifications from 'react-notifications-component';
-import React from 'react';
-import Header from './Header';
-import About from './About';
-import Projects from './Projects';
-import Background from './Background';
-import Skills from './Skills';
-import Contact from './Contact';
-import Footer from './Footer'
+import React from "react";
+import ReactNotifications from "react-notifications-component";
+import ReactGa from "react-ga";
+import Header from "./Header";
+import About from "./About";
+import Projects from "./Projects";
+import Background from "./Background";
+import Skills from "./Skills";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 class App extends React.Component {
+  componentDidMount() {
+    ReactGa.initialize("UA-159039430-1");
+    ReactGa.pageview("/");
+  }
   render() {
-      return (
-            <div>
-                <ReactNotifications />
-                <Header />
-                <Background />
-                <About id="about" />
-                <Skills id="skills" />
-                <Projects id="projects" />
-                <Contact id="contact"/>
-                <Footer />
-            </div>
-      )
-  } 
+    return (
+      <div>
+        <ReactNotifications />
+        <Header />
+        <Background />
+        <About id="about" />
+        <Skills id="skills" />
+        <Projects id="projects" />
+        <Contact id="contact" />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
